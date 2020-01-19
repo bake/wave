48aec5c1689573942811197604023c8eae0c5bd9
bake <bake@192k.pw>
2020-01-19 13:44:20 +0100

    Use binary.Read and Write for Format chunks

--- format.go
+++ format.go
@@ -3,9 +3,6 @@ package wave
 import (
 	"encoding/binary"
 	"io"
-	"io/ioutil"
-
-	"github.com/pkg/errors"
 )
 
 // Format holds configuration about the WAVE.
@@ -20,34 +17,12 @@ type Format struct {
 
 // decodeFormat decodes a chunk in a format chunk.
 func decodeFormat(r io.Reader) (Format, error) {
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		return Format{}, errors.Wrap(err, "could not read format chunk")
-	}
-	return Format{
-		AudioFormat:   binary.LittleEndian.Uint16(data[:2]),
-		NumChans:      binary.LittleEndian.Uint16(data[2:4]),
-		SampleRate:    binary.LittleEndian.Uint32(data[4:8]),
-		ByteRate:      binary.LittleEndian.Uint32(data[8:12]),
-		BlockAlign:    binary.LittleEndian.Uint16(data[12:14]),
-		BitsPerSample: binary.LittleEndian.Uint16(data[14:16]),
-	}, nil
+	var dst Format
+	err := binary.Read(r, binary.LittleEndian, &dst)
+	return dst, err
 }
 
 // encode a format struct into an io.Writer.
 func (f *Format) encode(w io.Writer) error {
-	var err error
-	write := func(data interface{}) {
-		if err != nil {
-			return
-		}
-		err = binary.Write(w, binary.LittleEndian, data)
-	}
-	write(f.AudioFormat)
-	write(f.NumChans)
-	write(f.SampleRate)
-	write(f.ByteRate)
-	write(f.BlockAlign)
-	write(f.BitsPerSample)
-	return err
+	return binary.Write(w, binary.LittleEndian, f)
 }
